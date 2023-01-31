ae817dc86381d9af7de8acb8e111dea1df7f87dd
barfra198919@gmail.com <barfra198919@gmail.com>
2023-01-31 20:48:51 +0100

    zadanie dzien 3stworzenie pustej aplikacji

---
+++ FirstApp/FirstApp/Program.cs
@@ -0,0 +1,20 @@
+﻿string name = "Ewa";
+var myAge = 33;
+char gender = 'f';
+
+if (gender == 'f' && myAge < 30)
+{
+    Console.WriteLine("Kobieta poznizej 30 lat");
+}
+else if (name == "Ewa" && myAge == 33 && gender == 'f')
+{
+    Console.WriteLine("Ewa, 33 lata");
+}
+else if (myAge < 18 && gender != 'f')
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+else
+{
+    Console.WriteLine("Brak danych");
+}
